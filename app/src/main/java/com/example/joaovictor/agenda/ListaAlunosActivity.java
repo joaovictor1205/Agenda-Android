2267f7a3a7ff64ae79604c8bbf46f6eec747c5d6
João Victor <joaovictor.lol12@gmail.com>
2018-03-11 16:37:51 -0300

    Continuação do código

--- app/src/main/java/com/example/joaovictor/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/joaovictor/agenda/ListaAlunosActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
