51265716729b5380db2f52caf5d848e291a6fde7
João Victor <joaovictor.lol12@gmail.com>
2018-03-13 20:05:57 -0300

    Continuação do código

--- app/src/main/java/com/example/joaovictor/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/joaovictor/agenda/FormularioActivity.java
@@ -1,5 +1,6 @@
 package com.example.joaovictor.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,7 @@ public class FormularioActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this, "Salvo!", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
